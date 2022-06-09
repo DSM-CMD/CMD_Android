be6db20fdac785d2617746799a8e6cfb66afef8e
Tmdhoon2 <hks026naver@gmail.com>
2022-06-09 17:04:18 +0900

    레이아웃 수정

--- CMD/app/src/main/java/com/tmdhoon/cmd/MainActivity.java
+++ CMD/app/src/main/java/com/tmdhoon/cmd/MainActivity.java
@@ -3,6 +3,10 @@ package com.tmdhoon.cmd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
