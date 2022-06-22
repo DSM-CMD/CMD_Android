1a54b569845382fe054ec1cea0914feebb59a8a0
Tmdhoon2 <hks026naver@gmail.com>
2022-06-22 19:06:17 +0900

    바텀 내비게이션 디자인

---
+++ CMD/app/src/main/java/com/tmdhoon/cmd/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.tmdhoon.cmd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tmdhoon.cmd.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
