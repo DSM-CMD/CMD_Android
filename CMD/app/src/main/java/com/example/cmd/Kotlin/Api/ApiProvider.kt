e8f6953084669615ee8201c089d832536047a5c9
Tmdhoon_2 <hks026naver@gmail.com>
2022-08-12 20:34:50 +0900

    🎮 :: 서버 인터페이스, 오브젝트 추가
    Test Code

---
+++ CMD/app/src/main/java/com/example/cmd/Kotlin/Api/ApiProvider.kt
@@ -0,0 +1,13 @@
+package com.example.cmd.Kotlin.Api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiProvider {
+    var BASE_URL = "http://54.180.122.62:8080/"
+    var retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ServerApi::class.java)
+}
