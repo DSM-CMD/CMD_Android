30e469426e7943fb6e7d425e8cf0ea8e256d75a2
Tmdhoon_2 <hks026naver@gmail.com>
2022-08-16 11:56:28 +0900

    🎮 :: 코틀린 Wating Activity Animation 추가

--- CMD/app/src/main/java/com/example/cmd/Kotlin/Main/WatingActivity2.kt
+++ CMD/app/src/main/java/com/example/cmd/Kotlin/Main/WatingActivity2.kt
@@ -3,17 +3,28 @@ package com.example.cmd.Kotlin.Main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import com.example.cmd.databinding.ActivityWatingBinding
 
 class WatingActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityWatingBinding
+    lateinit var anim:Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityWatingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        anim = AlphaAnimation(0.0f, 1.0f)
+        anim.duration = 2000
+        anim.startOffset = 20
+        anim.repeatMode = Animation.REVERSE
+        anim.repeatCount = Animation.INFINITE
+
+        binding.tvtouch.startAnimation(anim)
 
         binding.wating.setOnClickListener{
             val intent = Intent(applicationContext, LobbyActivity2::class.java)
