2d5f234f718b32c4ee2a60cee4fbae3ac73473b1
Tmdhoon_2 <hks026naver@gmail.com>
2022-08-19 22:11:50 +0900

    📝 :: findViewById -> viewBinding 변경

--- CMD/app/src/main/java/com/example/cmd/Fragment/TimetableFragment.java
+++ CMD/app/src/main/java/com/example/cmd/Fragment/TimetableFragment.java
@@ -15,6 +15,7 @@ import com.example.cmd.Api.ServerApi;
 import com.example.cmd.Main.SignInActivity;
 import com.example.cmd.R;
 import com.example.cmd.Response.TimetableResponse;
+import com.example.cmd.databinding.FragmentTimetableBinding;
 
 import org.w3c.dom.Text;
 
@@ -29,25 +30,13 @@ import retrofit2.Response;
 
 public class TimetableFragment extends Fragment {
 
-    private TextView tvdate;
-    private TextView period01;
-    private TextView period02;
-    private TextView period03;
-    private TextView period04;
-    private TextView period05;
-    private TextView period06;
-    private TextView period07;
-    private TextView period08;
-    private TextView period09;
-    private TextView period10;
-
+    private FragmentTimetableBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_timetable, container, false);
 
-        tvdate = rootView.findViewById(R.id.tvdate);
+        binding = FragmentTimetableBinding.inflate(getLayoutInflater(), container, false);
 
         Date currentDate = new Date();
 
@@ -57,20 +46,7 @@ public class TimetableFragment extends Fragment {
 
         String format_time = format.format(calendar.getTime());
 
-        tvdate.setText(format_time);
-
         calendar.setTime(currentDate);
-
-        period01 = rootView.findViewById(R.id.tvperiod01);
-        period02 = rootView.findViewById(R.id.tvperiod02);
-        period03 = rootView.findViewById(R.id.tvperiod03);
-        period04 = rootView.findViewById(R.id.tvperiod04);
-        period05 = rootView.findViewById(R.id.tvperiod05);
-        period06 = rootView.findViewById(R.id.tvperiod06);
-        period07 = rootView.findViewById(R.id.tvperiod07);
-        period08 = rootView.findViewById(R.id.tvperiod08);
-        period09 = rootView.findViewById(R.id.tvperiod09);
-        period10 = rootView.findViewById(R.id.tvperiod10);
 
         ServerApi serverApi = ApiProvider.getInstance().create(ServerApi.class);
 
@@ -78,16 +54,16 @@ public class TimetableFragment extends Fragment {
             @Override
             public void onResponse(Call<TimetableResponse> call, Response<TimetableResponse> response) {
                 if(response.isSuccessful()){
-                    period01.setText(response.body().getPeriod1st());
-                    period02.setText(response.body().getPeriod2nd());
-                    period03.setText(response.body().getPeriod3th());
-                    period04.setText(response.body().getPeriod4th());
-                    period05.setText(response.body().getPeriod5th());
-                    period06.setText(response.body().getPeriod6th());
-                    period07.setText(response.body().getPeriod7th());
-                    period08.setText(response.body().getPeriod8th());
-                    period09.setText(response.body().getPeriod9th());
-                    period10.setText(response.body().getPeriod10th());
+                    binding.tvperiod01.setText(response.body().getPeriod1st());
+                    binding.tvperiod02.setText(response.body().getPeriod2nd());
+                    binding.tvperiod03.setText(response.body().getPeriod3th());
+                    binding.tvperiod04.setText(response.body().getPeriod4th());
+                    binding.tvperiod05.setText(response.body().getPeriod5th());
+                    binding.tvperiod06.setText(response.body().getPeriod6th());
+                    binding.tvperiod07.setText(response.body().getPeriod7th());
+                    binding.tvperiod08.setText(response.body().getPeriod8th());
+                    binding.tvperiod09.setText(response.body().getPeriod9th());
+                    binding.tvperiod10.setText(response.body().getPeriod10th());
                 }
             }
 
@@ -97,6 +73,6 @@ public class TimetableFragment extends Fragment {
             }
         });
 
-        return rootView;
+        return binding.getRoot();
     }
 }
