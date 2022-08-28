2145202f7bef287ee19f5f905bfc1eb6a2ae083f
Tmdhoon2 <hks026naver@gmail.com>
2022-08-28 20:38:30 +0900

    📱 :: recyclerview 디자인 변경

--- CMD/app/src/main/java/com/example/cmd/Response/UserPostResponse.java
+++ CMD/app/src/main/java/com/example/cmd/Response/UserPostResponse.java
@@ -4,11 +4,13 @@ public class UserPostResponse {
     private long id;
     private String title;
     private String contents;
+    private String writer;
 
-    public UserPostResponse(long id, String title, String contents) {
+    public UserPostResponse(long id, String title, String contents, String writer) {
         this.id = id;
         this.title = title;
         this.contents = contents;
+        this.writer = writer;
     }
 
     public long getId() {
@@ -21,5 +23,9 @@ public class UserPostResponse {
 
     public String getContents() {
         return contents;
+    }
+
+    public String getWriter(){
+        return writer;
     }
 }
